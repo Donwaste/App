a19496e2c05d85e44491aa57b5bde6ac845cf1f7
Donwaste <blazenkodima@gmail.com>
2025-02-03 20:23:21 +0200

    -

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -1,44 +1,90 @@
 import User from "./user";
 import Pagination from "./pagination";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { paginate } from "../utils/paginate";
+import GroupList from "./groupLIst";
+import SearchStatus from "./searchStatus";
+import API from "../api";
+
 const Users = ({ users, ...rest }) => {
-  const count = users.length;
-  const pageSize = 4;
+  const pageSize = 2;
+  const [professions, setProfession] = useState();
   const [currentPage, setCurrentPage] = useState(1);
+  const [selectedProf, setSelectedProf] = useState(null);
+
+  useEffect(() => {
+    API.professions.fetchAll().then((data) => setProfession(data));
+  }, []);
+
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [selectedProf]);
+
+  const filteredUsers = selectedProf
+    ? users.filter((user) => user.profession._id === selectedProf._id)
+    : users;
+
+  const count = filteredUsers.length;
+
+  const handleProfessionSelect = (item) => {
+    setSelectedProf((prev) => (prev?._id === item._id ? null : item));
+  };
+
   const handlePageChange = (pageIndex) => {
     setCurrentPage(pageIndex);
   };
-  const userCorp = paginate(users, currentPage, pageSize);
+
+  const userCorp = paginate(filteredUsers, currentPage, pageSize);
+  const clearFilter = () => {
+    setSelectedProf();
+  };
+
   return (
-    <>
-      {count > 0 && (
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Имя</th>
-              <th scope="col">Качества</th>
-              <th scope="col">Провфессия</th>
-              <th scope="col">Встретился, раз</th>
-              <th scope="col">Оценка</th>
-              <th scope="col">Избранное</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            {userCorp.map((user) => (
-              <User key={user._id} {...rest} {...user} />
-            ))}
-          </tbody>
-        </table>
+    <div className="d-flex">
+      {professions && (
+        <div className="d-flex flex-column flex-shrink-0 p-3">
+          <GroupList
+            items={professions}
+            onItemSelect={handleProfessionSelect}
+            selectedItem={selectedProf}
+          />
+          <button className="btn btn-secondary mt-2 " onClick={clearFilter}>
+            Очистить
+          </button>
+        </div>
       )}
-      <Pagination
-        itemsCount={count}
-        pageSize={pageSize}
-        currentPage={currentPage}
-        onPageChange={handlePageChange}
-      />
-    </>
+      <div className="d-flex flex-column">
+        <SearchStatus length={count} />
+        {count > 0 && (
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Имя</th>
+                <th scope="col">Качества</th>
+                <th scope="col">Профессия</th>
+                <th scope="col">Встретился, раз</th>
+                <th scope="col">Оценка</th>
+                <th scope="col">Избранное</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {userCorp.map((user) => (
+                <User key={user._id} {...rest} {...user} />
+              ))}
+            </tbody>
+          </table>
+        )}
+        <div className="d-flex justify-content-center">
+          <Pagination
+            itemsCount={count}
+            pageSize={pageSize}
+            currentPage={currentPage}
+            onPageChange={handlePageChange}
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 
